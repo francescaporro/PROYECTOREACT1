3b39dfba2530858a59ba54ede3e2e6804c4a379a
francescaporro <50020152+francescaporro@users.noreply.github.com>
2023-04-03 19:35:45 -0300

    footer

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Cards from './components/Cards/Cards'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
+    <>
     <div className="App">
       <Cards/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ proyecto-react/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css';
+function Footer(){
+    return(
+        <footer>
+            <p>Martina Arnaude   &   Francesca Porro Toyos</p>
+        </footer>
+    )
+}
+
+export default Footer
